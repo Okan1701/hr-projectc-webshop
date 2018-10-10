9cda5ae225215117349a39474e9ff2645d06da46
DESKTOP-6P67NTL\geord <geordycjd@gmail.com>
2018-10-10 15:32:02 +0200

    top category animated

--- ClientApp/src/InteractiveImage.js
+++ ClientApp/src/InteractiveImage.js
@@ -5,14 +5,6 @@ import boat from './images/boat.png';
 import category from './images/category.png';
 import Searchbar from './Searchbar';
 import './index.css';
-import Animate from 'react-move/Animate';
-import { easeExpOut } from 'd3-ease';
-import Fade from 'react-reveal/Fade';
-import Rotate from 'react-reveal/Rotate';
-import Slide from 'react-reveal/Slide';
-import { Spring } from 'react-spring'
-import classNames from 'classnames/bind';
-
 
 export class InteractiveImage extends Component {
   
@@ -33,8 +25,20 @@ export class InteractiveImage extends Component {
 
       <div id="container">
 
-        <div id="three-container" >
-          <Image src={category} id="image" className={this.state.show ? 'slide-tl' : 'slide-tl2'} />
+        <div>
+          <Image src={category} id="image" className={this.state.show ? 'slide-tl' : 'slide-tl-reverse'} />
+        </div>
+
+        <div>
+          <Image src={category} id="image2" className={this.state.show ? 'slide-tl2' : 'slide-tl-reverse2'} />
+        </div>
+
+         <div>
+          <Image src={category} id="image3" className={this.state.show ? 'slide-tl3' : 'slide-tl-reverse3'} />
+        </div>
+
+        <div>
+          <Image src={category} id="image4" className={this.state.show ? 'slide-tl4' : 'slide-tl-reverse4'} />
         </div>
         
       <div id="boat">
@@ -47,152 +51,3 @@ export class InteractiveImage extends Component {
     );
   }
 }
-
-        {/* <div>
-
-          <Fade bottom when={this.state.show}>
-            <img src={category} id="image" />
-          </Fade>
-          
-          <Fade bottom when={this.state.show}>
-            <img src={category} id="image2" />
-          </Fade>
-
-          <Fade bottom when={this.state.show}>
-            <img src={category} id="image3" />
-          </Fade>
-
-          <Fade bottom when={this.state.show}>
-            <img src={category} id="image4" />
-          </Fade>
-          
-        </div> */}
-
-
-// export class InteractiveImage extends Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {isToggleOn: false};
-
-//     // This binding is necessary to make `this` work in the callback
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     this.setState(state => ({
-//       isToggleOn: !state.isToggleOn
-//     }));
-//   }
-
-//   render() {
-
-//     return (
-//       <div>
-
-//         <div >
-
-//           {this.state.isToggleOn ? (
-//             <Rotate top left>
-//               <img src={category} id="image" />
-//             </Rotate>
-//           ) : null }
-
-//           {!this.state.isToggleOn ? (
-//             <Slide right>
-//               {/* <img src={category} id="image" /> */}
-//             </Slide>
-//           ) : null }
-
-//           {this.state.isVisible ? (
-//             <Fade bottom>
-//               <img src={category} id="image2" />
-//             </Fade>
-//           ) : null}
-
-//           {this.state.isVisible ? (
-//             <Fade bottom>
-//               <img src={category} id="image3" />
-//             </Fade>
-//           ) : null}
-
-//           {this.state.isVisible ? (
-//             <Rotate top right>
-//               <img src={category} id="image4" />
-//             </Rotate>
-//           ) : null}
-
-//         </div>
-
-//       <div >
-     
-//         <Image src={boat} id="boat" onClick={this.handleClick} />
-//           {console.log(this.state.isToggleOn ? 'ON' : 'OFF')}
-        
-//       </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export class InteractiveImage extends Component {
-
-//   constructor() {
-//     super();
-//     this.state = {
-//       isVisible: true,
-//     };
-//   }
-
-//   handleEvent = () => {
-//     console.log("I was clicked");
-//   };
-
-//   render() {
-
-//     return (
-//       <div responsive >
-
-//         <div >
-
-//           {this.state.isVisible ? (
-//             <Rotate top left>
-//               <img src={category} id="image" />
-//             </Rotate>
-//           ) : null }
-
-//           {this.state.isVisible ? (
-//             <Fade bottom>
-//               <img src={category} id="image2" />
-//             </Fade>
-//           ) : null}
-
-//           {this.state.isVisible ? (
-//             <Fade bottom>
-//               <img src={category} id="image3" />
-//             </Fade>
-//           ) : null}
-
-//           {this.state.isVisible ? (
-//             <Rotate top right>
-//               <img src={category} id="image4" />
-//             </Rotate>
-//           ) : null}
-
-//         </div>
-
-//       <div >
-     
-        
-//         <Image src={boat} id="boat" onClick={() => this.setState({
-//           isVisible: !this.state.isVisible
-//         })} />
-
-
-
-
-//       </div>
-//       </div>
-//     );
-//   }
-// }
